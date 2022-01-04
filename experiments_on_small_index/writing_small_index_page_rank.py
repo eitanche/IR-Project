82dc1b87d5666a4c72793694d6beaec94110694c
Ehud <ehudcode@gmail.com>
2022-01-05 01:05:01 +0200

    only fronted left

---
+++ experiments_on_small_index/writing_small_index_page_rank.py
@@ -0,0 +1,41 @@
+import csv
+import json
+from collections import defaultdict
+
+from inverted_index_gcp import InvertedIndex
+
+
+need_words = ['python', 'data', 'science', 'migraine', 'chocolate', 'how', 'to', 'make', 'pasta', 'Does', 'pasta', 'have', 'preservatives?', 'how', 'google', 'works', 'what', 'is', 'information', 'retrieval', 'NBA', 'yoga', 'how', 'to', 'not', 'kill', 'plants', 'masks', 'black', 'friday', 'why', 'do', 'men', 'have', 'nipples', 'rubber', 'duck', 'michelin', 'what', 'to', 'watch', 'best', 'marvel', 'movie', 'how', 'tall', 'is', 'the', 'eiffel', 'tower', 'where', 'does', 'vanilla', 'flavoring', 'come', 'from', 'best', 'ice', 'cream', 'flavour', 'how', 'to', 'tie', 'a', 'tie', 'how', 'to', 'earn', 'money', 'online', 'what', 'is', 'critical', 'race', 'theory', 'what', 'space', 'movie', 'was', 'made', 'in', '1992', 'how', 'to', 'vote', 'google', 'trends', 'dim', 'sum', 'ted', 'fairy', 'tale']
+mydict = {}
+
+index = InvertedIndex.read_index("small_indexes/small_body_index", "postings_gcp_index")
+with open('C:/Users/Owner/PycharmProjects/IR-Project/pr_part-00000-81ea836c-2783-47f3-8489-9ccc7ae13f8a-c000.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    mydict = {rows[0]:rows[1] for rows in reader if rows[0]}
+    # print(list(mydict.items())[0])
+    new_dict = defaultdict(list)
+    for term in need_words:
+        # try:
+        if term in index.df:
+            terms_from_pls = index.read_posting_list(term)
+            for pls in terms_from_pls:
+                # print(pls[0])
+                if str(pls[0]) in mydict.keys():
+                    # print("HERE")
+                    new_dict[pls[0]] = mydict[str(pls[0])]
+            # except Exception as e:
+            #     print(e)
+            #     # continue
+
+    print(list(new_dict.items())[:10])
+
+
+
+
+
+
+    with open('small_page_rank.json', mode='w') as outfile:
+        json.dump(mydict,outfile)
+
+
+
