cbd8355327bc8386477a4376e2d31771da8c3c6b
Eitan-Chertok <eitan.chertok@ibm.com>
2022-01-04 16:11:40 +0200

    X

--- create_small_index/get_query_terms.py
+++ create_small_index/get_query_terms.py
@@ -1,7 +1,8 @@
 import json
+from nltk.stem.porter import *
 
 with open("queries_train.json") as f:
     queries = json.load(f)
-
-query_terms = [term for query in queries for term in query.split(" ")]
+stemmer = PorterStemmer()
+query_terms = [stemmer.stem(term) for query in queries for term in query.split(" ")]
 print(query_terms)
