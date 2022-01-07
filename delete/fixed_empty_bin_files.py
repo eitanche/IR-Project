1d675db4fa377067df16c977294174d5e0ff83fc
Eitan-Chertok <eitan.chertok@ibm.com>
2022-01-07 14:31:47 +0200

    fixed empty bin files

--- delete/fixed_empty_bin_files.py
+++ delete/fixed_empty_bin_files.py
@@ -39,7 +39,7 @@ class MultiFileWriter:
             remaining = BLOCK_SIZE - pos
             # if the current file is full, close and open a new one.
             if remaining == 0:
-                self._f.close()
+                #self._f.close()
                 self.upload_to_gcp()
                 self._f = next(self._file_gen)
                 pos, remaining = 0, BLOCK_SIZE
@@ -56,8 +56,10 @@ class MultiFileWriter:
             The function saves the posting files into the right bucket in google storage.
         '''
         file_name = self._f.name
-        blob = self.bucket.blob(f"postings_small_anchor_text_gcp/{file_name}")
-        blob.upload_from_filename(file_name)
+        self._f.close()
+        blob = self.bucket.blob(f"last_title_test/{file_name}")
+        with open(self._base_dir / f'{file_name}', "rb") as f:
+          blob.upload_from_file(f)
 
     @staticmethod
     def do_it():
@@ -206,7 +208,6 @@ class InvertedIndex:
             pickle.dump(posting_locs, f)
         client = storage.Client()
         bucket = client.bucket(bucket_name)
-        blob_posting_locs = bucket.blob(f"postings_small_anchor_text_gcp/{bucket_id}_posting_locs.pickle")
+        blob_posting_locs = bucket.blob(f"last_title_test/{bucket_id}_posting_locs.pickle")
         blob_posting_locs.upload_from_filename(f"{bucket_id}_posting_locs.pickle")
-
 
