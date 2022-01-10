73bca90eea745085d17df4456b5a7921b04b0d21
Ehud <ehudcode@gmail.com>
2022-01-10 14:18:29 +0200

    run over tf_idf pls

---
+++ front_end/iterate_all_postings_tf_idf.py
@@ -0,0 +1,10 @@
+from body_tf_idf_inverted_index_gcp_without_stemming import InvertedIndex
+
+
+def run_all_pls_in_index(base_dir, bucket_name):
+    index = InvertedIndex.read_index(bucket_name,base_dir,"index")
+    for word in index.df:
+        for pls in index.read_posting_list(word, "anchor_binary_index"):
+            x=1
+
+    print(f"index: {base_dir} is ok")
